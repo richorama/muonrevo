4d9a21eefe38f323eee161e80115a6fbe5bc15d9
Richard Astbury <richard.astbury@gmail.com>
2019-09-23 14:11:20 +1000

    add welcome page

--- components/confirm-redirect-page.jsx
+++ components/confirm-redirect-page.jsx
@@ -5,26 +5,25 @@ var Panel = require('./panel.jsx')
 module.exports = class extends React.Component {
   render() {
     return (
-      <Page>
-        <Panel title="Welcome to MuonRevo">
-          <div>
-            <p>
-              MuonRevo is a markdown based note taking application, which uses
-              dropbox for storage.
-            </p>
-            <p>
-              The application requires access to your dropbox account, where it
-              will read and write files in an 'Apps' folder. It will not have
-              access to any other files in your account.
-            </p>
-            <p>
-              <a href={this.props.url} className="btn btn-lg btn-primary">
-                <i className="icon-social-dropbox" /> Log in with dropbox
-              </a>
-            </p>
-          </div>
-        </Panel>
-      </Page>
+      <div style={{ paddingTop: 20 }}>
+        <div className="ui segment inverted container" style={{ fontSize: 24 }}>
+          <h1>👋 Welcome!</h1>
+          <p>
+            MuonRevo is a markdown based note taking application, which uses
+            dropbox for storage.
+          </p>
+          <p>
+            The application requires access to your dropbox account, where it
+            will read and write files in an 'Apps' folder. It will not have
+            access to any other files in your account.
+          </p>
+          <p>
+            <a href={this.props.url} className="huge ui blue button">
+              <i className="ui icon dropbox" /> Log in with dropbox
+            </a>
+          </p>
+        </div>
+      </div>
     )
   }
 }
